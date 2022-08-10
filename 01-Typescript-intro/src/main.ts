28be5add42e88aac13c859929e258330c4e906c0
Mike <lanselot777hailander@gmail.com>
2022-08-10 10:22:09 -0400

    Decorators

--- 01-Typescript-intro/src/main.ts
+++ 01-Typescript-intro/src/main.ts
@@ -1,8 +1,10 @@
-import { charmander } from './bases/04-injection'
+import { charmander } from './bases/06-decorators'
 import './style.css'
 //import { pokemons } from './bases/02-objects'
 // import {name, age} from './bases/01-types';
 // import { charmander } from './bases/03-clases'
+// import { charmander } from './bases/04-injection'
+// import { charmander } from './bases/05-decoradores'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hello ${ charmander.name }!!!</h1>
