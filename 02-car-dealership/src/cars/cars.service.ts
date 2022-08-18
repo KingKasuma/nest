303b865ff47782aa43261eaf8eaa93a5d53f7c19
Mike <lanselot777hailander@gmail.com>
2022-08-18 17:02:08 -0400

    Seed

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -7,21 +7,11 @@ import { Car } from './interfaces/car.interface';
 export class CarsService {
 
     private cars: Car[] = [
-        {
-            id: uuid(),
-            brand: 'Toyota',
-            model: 'Corolla'
-        },
-        {
-            id: uuid(),
-            brand: 'Honda',
-            model: 'Civic'
-        },
-        {
-            id: uuid(),
-            brand: 'Jeep',
-            model: 'Cherokee'
-        }
+        // {
+        //     id: uuid(),
+        //     brand: 'Toyota',
+        //     model: 'Corolla'
+        // }
     ];
 
     findAll() {
@@ -76,5 +66,9 @@ export class CarsService {
         const car = this.findOneByid( id );
         this.cars = this.cars.filter( car => car.id !== id );
         return; //undefined
+    }
+
+    fillCarsWithSeedData( cars: Car[] ) {
+        this.cars = cars;
     }
 }
---
+++ 02-car-dealership/src/seed/seed.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { BRANDS_SEED } from './data/brands.seed';
+import { CARS_SEED } from './data/cars.seed';
+
+@Injectable()
+export class SeedService {
+  
+  populateDB() {
+
+    // CARS_SEED
+    // BRANDS_SEED
+
+    return 'Seed executed successfully';
+  }
+}
