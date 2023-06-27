55ecab196cfa7266d71a7a8d63d1ae46d1259e38
Mike <lanselot777hailander@gmail.com>
2023-06-26 23:14:49 -0400

    Auth

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -3,12 +3,39 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/User.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([ User ])
+    TypeOrmModule.forFeature([ User ]),
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        // console.log('JWT_SECRET', configService.get('JWT_SECRET'))
+        // console.log('JWT_SECRET', process.env.JWT_SECRET)
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    }),
+
+    /* JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '2h'
+      }
+    }) */
   ],
   exports: [ TypeOrmModule ]
 })
--- 04-teslo-shop/src/auth/auth.service.ts
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from "bcrypt";
 import { User } from './entities/User.entity';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -16,9 +17,15 @@ export class AuthService {
     
     try {
 
-      const user = this.userRepository.create( createUserDto );
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync( password, 10 )
+      });
 
       await this.userRepository.save(user)
+      delete user.password;
 
       return user;
 
@@ -26,6 +33,25 @@ export class AuthService {
       this.handleDBErrors(error);
     }
 
+  }
+
+  async login( loginUserDto: LoginUserDto ) {
+
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    });
+
+    if ( !user )
+      throw new UnauthorizedException('Credentials are not valid (email)');
+    
+    if ( !bcrypt.compareSync(password, user.password) )
+      throw new UnauthorizedException('Credentials are not valid (password)');
+
+    return user;
+    
   }
 
   private handleDBErrors(error: any): never {
@@ -36,4 +62,6 @@ export class AuthService {
 
       throw new InternalServerErrorException('Please check server logs');
   }
+
+
 }
